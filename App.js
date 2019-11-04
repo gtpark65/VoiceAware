32ace67d7a4eed11c5b93a25b6508a543ef93865
gpark65 <gpark65@gmail.com>
2019-11-04 14:39:58 -0500

    enabled ble sync with micro controller

--- App.js
+++ App.js
@@ -8,13 +8,12 @@ export default class SensorsComponent extends Component {
     super()
     this.manager = new BleManager()
     this.state = {info: "", values: {}}
-    this.prefixUUID = "f00011"
-    this.suffixUUID = "-0451-4000-b000-000000000000"
+    this.prefixUUID = "F00011"
+    this.suffixUUID = "-0451-4000-B000-000000000000"
     this.sensors = {
-      0: "LED",
-      1: "LED1",
+      1: "LED",
       2: "Button",
-      3: "Data Service"
+      3: "Data"
     }
   }
 
@@ -23,13 +22,12 @@ export default class SensorsComponent extends Component {
   }
 
   notifyUUID(num) {
-    return this.prefixUUID + num + "1" + this.suffixUUID
-  }
-
-  writeUUID(num) {
     return this.prefixUUID + num + "2" + this.suffixUUID
   }
 
+  writeUUID(num) {
+    return this.prefixUUID + num + "1" + this.suffixUUID
+  }
   info(message) {
     this.setState({info: message})
   }
@@ -41,7 +39,6 @@ export default class SensorsComponent extends Component {
   updateValue(key, value) {
     this.setState({values: {...this.state.values, [key]: value}})
   }
-  
   componentWillMount() {
     if (Platform.OS === 'ios') {
       this.manager.onStateChange((state) => {
@@ -62,10 +59,9 @@ export default class SensorsComponent extends Component {
         return
       }
 
-      if (device.name === 'Project Zero') {
-        this.info("Connecting to Project Zero")
+      if (device.name === 'ProjectZero') {
+        this.info("Connecting to TI Sensor")
         this.manager.stopDeviceScan()
-        
         device.connect()
           .then((device) => {
             this.info("Discovering services and characteristics")
@@ -83,7 +79,6 @@ export default class SensorsComponent extends Component {
       }
     });
   }
-  //callback 
   async setupNotifications(device) {
     for (const id in this.sensors) {
       const service = this.serviceUUID(id)
@@ -91,10 +86,11 @@ export default class SensorsComponent extends Component {
       const characteristicN = this.notifyUUID(id)
 
       const characteristic = await device.writeCharacteristicWithResponseForService(
-        service, characteristicW, "AQ==" /* 0x01 in hex */
+        service, characteristicW, "AQ==" /* 0x01*/
       )
-
-      device.monitorCharacteristicForService(service, characteristicN, (error, characteristic) => {
+      
+      this.info("write function executed")
+      device.monitorCharacteristicForService(service, characteristicW, (error, characteristic) => {
         if (error) {
           this.error(error.message)
           return
@@ -105,13 +101,9 @@ export default class SensorsComponent extends Component {
   }
   render() {
     return (
-      <View style = {{padding: 50}} >
+      <View style = {{padding: 50}}>
         <Text>{this.state.info}</Text>
-        {Object.keys(this.sensors).map((key) => {
-          return <Text key={key}>
-                   {this.sensors[key] + ": " + (this.state.values[this.notifyUUID(key)] || "-")}
-                 </Text>
-        })}
+      
       </View>
     )
   }
